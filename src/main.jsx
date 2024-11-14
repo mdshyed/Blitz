f0a5ab80d54aefc4a9f4f3af8f74d572cf198acd
mdshyed <167856124+mdshyed@users.noreply.github.com>
2024-11-14 21:38:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,11 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+
     <Provider store={store}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
+
 );
